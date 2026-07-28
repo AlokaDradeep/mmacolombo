d18130e0694c90b80e7a17b12c1301a8d5092f65
Aloka Pradeep <alokapradeep01@gmail.com>
2026-07-28 16:51:20 +0530

    added new files and modified everything

--- js/main.js
+++ js/main.js
@@ -83,58 +83,6 @@
         }
     }
 
-    function initSnowEffect() {
-        var snowContainer = document.getElementById('snow-container');
-        if (!snowContainer) {
-            return;
-        }
-
-        var snowContent = ['&#10052', '&#10053', '&#10054'];
-        var snowRemoved = false;
-
-        function random(limit) {
-            return Math.floor(Math.random() * limit);
-        }
-
-        function getRandomStyles() {
-            var top = random(100);
-            var left = random(100);
-            var duration = random(10) + 10;
-            var size = random(25) + 25;
-            return [
-                'top: -' + top + '%;',
-                'left: ' + left + '%;',
-                'font-size: ' + size + 'px;',
-                'animation-duration: ' + duration + 's;'
-            ].join(' ');
-        }
-
-        function removeSnow() {
-            if (snowRemoved || !snowContainer.parentNode) {
-                return;
-            }
-
-            snowRemoved = true;
-            snowContainer.style.opacity = '0';
-            window.setTimeout(function () {
-                if (snowContainer.parentNode) {
-                    snowContainer.parentNode.removeChild(snowContainer);
-                }
-            }, 500);
-        }
-
-        for (var i = 0; i < 30; i += 1) {
-            var snow = document.createElement('div');
-            snow.className = 'snow';
-            snow.style.cssText = getRandomStyles();
-            snow.innerHTML = snowContent[random(snowContent.length)];
-            snowContainer.appendChild(snow);
-        }
-
-        window.setTimeout(removeSnow, 60000);
-        window.addEventListener('click', removeSnow, { once: true });
-    }
-
     hideSpinner();
     initWow();
     initStickyNavbar();
@@ -208,5 +156,4 @@
         }
     });
     initCounterUp();
-    initSnowEffect();
 })(jQuery);
